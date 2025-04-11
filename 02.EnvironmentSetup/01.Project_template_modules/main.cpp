c60ab4383ce42988c34f5e70b172c4ae089f450e
Andy Bellenhaus <andybellenhaus@gmail.com>
2025-04-11 12:10:30 +0200

    Template int main function

--- 02.EnvironmentSetup/01.Project_template_modules/main.cpp
+++ 02.EnvironmentSetup/01.Project_template_modules/main.cpp
@@ -2,15 +2,13 @@
     . Project description
         . Topic #1
         . Topic #2
+*/
 
 //This will comment out the entire line
  #include <fmt/format.h>
  import utilities;
 
 int main(){
-    int value1 {110};
-    int value2 {30};
-    int result = value1 + value2;
-    fmt::print("Result: {}\n", result);
+    return 0;
 }
-    */
+    
